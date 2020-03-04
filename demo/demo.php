d640ad373abd570a83ff6d01b1254728fce31456
Grant Anderson <grant.anderson@gpsinsight.com>
2020-03-04 10:23:23 -0700

    initial commit

---
+++ demo/demo.php
@@ -0,0 +1,30 @@
+<?php
+
+use Pendenga\Log;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Psr\Log\NullLogger;
+include_once '../vendor/autoload.php';
+
+class Foo
+{
+    use LoggerAwareTrait;
+
+    public function __construct(LoggerInterface $logger = null)
+    {
+        $this->setLogger($logger ?? new NullLogger());
+    }
+
+    public function bar() {
+        $this->logger->debug(__METHOD__);
+        // ...
+        $this->logger->error(__METHOD__ . " Something Important!", ['code' => 123]);
+    }
+}
+
+$foo = new Foo(new Log\EchoLogger());
+$foo->bar();
+echo "---\n";
+$foo->setLogger(new Log\EchoLogger(LogLevel::ERROR));
+$foo->bar();
